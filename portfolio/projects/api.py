8e49ba4172b79fe6f5337dba248a9953121d285f
Kien Luu <kienluu@kien56.local>
2013-01-06 01:11:22 +0000

    include camelCase serializer
    Signed-off-by: Kien Luu <kienluu@kien56.local>

--- portfolio/projects/api.py
+++ portfolio/projects/api.py
@@ -1,13 +1,19 @@
+from tastypie import fields
 from tastypie.resources import ModelResource
 from portfolio.projects.models import Project, Group
+from portfolio.projects.serializers import camel_case_json_serializer
 
 class ProjectResource(ModelResource):
     class Meta:
         queryset = Project.objects.all()
         allowed_methods = ['get']
+        serializer = camel_case_json_serializer
 
 
 class GroupResource(ModelResource):
     class Meta:
         queryset = Group.objects.all()
         allowed_methods = ['get']
+        serializer = camel_case_json_serializer
+
+    projects = fields.ManyToManyField(ProjectResource,'projects', full=True)
